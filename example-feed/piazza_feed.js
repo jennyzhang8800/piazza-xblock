8790411fc32ae0a0577413eeba7c3a5ecd5ae845
jennyzhang8800 <jennyzhang8800@163.com>
2015-09-11 17:03:20 +0800

    add example together

--- example-feed/piazza_feed.js
+++ example-feed/piazza_feed.js
@@ -1,23 +1,59 @@
 
-$(document).ready(function(){
 
+$(document).ready(function(){
+    
     //注册一个比较大小的Helper,判断v1是否等于于v2
     Handlebars.registerHelper("compare",function(v1,v2,options){
-    if(v1==v2){
-    //满足添加继续执行
-    return options.fn(this);
-    }
-    else{
-    //不满足条件执行{{else}}部分
-         return options.inverse(this);
-     }
+        if(v1==v2){
+            //满足添加继续执行
+            return options.fn(this);
+        }
+        else{
+            //不满足条件执行{{else}}部分
+            return options.inverse(this);
+        }
     });
-   //feed
+
+     //页面加载时feed部分显示的是my_feed的内容 
+     url_github="https://raw.githubusercontent.com/xyongcn/piazza-data-tsinghua.edu.cn_spring2015_30240243x/master/data/piazza-data-filter/piazza_my_feed.txt";
+            $.ajax({
+                type : "get",
+                cache : false,
+                url : url_github , // 请求地址
+                success : function(data) { // ajax执行成功后执行的方法
+                    var data_json = eval("(" + data + ")"); // 把string转化为json
+                    var source_feed = $("#feed-template").html();
+                    var template_feed = Handlebars.compile(source_feed);
+                    var html_feed = template_feed(data_json);
+                    $("#feed").html(html_feed);}
+            });
+    
+     
+      //页面加载时popular_tags_bar部分显示的是popular tags的内容 
+    
+      url_github="https://raw.githubusercontent.com/xyongcn/piazza-data-tsinghua.edu.cn_spring2015_30240243x/master/data/piazza-data-filter/piazza_my_feed.txt";
+            $.ajax({
+                type : "get",
+                cache : false,
+                url : url_github , // 请求地址
+                success : function(data) { // ajax执行成功后执行的方法
+                    var data_json = eval("(" + data + ")"); // 把string转化为json
+                    var source_feed = $("#popular-tags-bar-template").html();
+                    var template_feed = Handlebars.compile(source_feed);
+                    var html_feed = template_feed(data_json);
+                    $("#popular_tags_bar").html(html_feed);}
+            });
+     
+
+   
+
+});
 
 
-    $("ul li a").bind("click",function()
-        {
-            var label=$(this).html();
+
+//点击列出的标签，显示对应的feed部分
+   function click_tags(label){
+           
             url_github="https://raw.githubusercontent.com/xyongcn/piazza-data-tsinghua.edu.cn_spring2015_30240243x/master/data/piazza-data-filter/filter_feed_"+label +".json";
             $.ajax({
                 type : "get",
@@ -31,24 +67,24 @@ $(document).ready(function(){
                     $("#feed").html(html_feed);}
             });
         }
-    );
-
-    $("#btn_read").click(function(){
-        var label = document.getElementById("label").value;
-        url_github="https://raw.githubusercontent.com/xyongcn/piazza-data-tsinghua.edu.cn_spring2015_30240243x/master/data/piazza-data-filter/filter_feed_"+label +".json";
-        $.ajax({
-            type : "get",
-            cache : false,
-            url : url_github , // 请求地址
-            success : function(data) { // ajax执行成功后执行的方法
-                var data_json = eval("(" + data + ")"); // 把string转化为json
-                var source_feed = $("#feed-template").html();
-                var template_feed = Handlebars.compile(source_feed);
-                var html_feed = template_feed(data_json);
-                $("#feed").html(html_feed);}
-        });
-    });
 
 
-});
+
+//下拉菜单列出所有标签,选择后显示在feed部分
+function click_select_label(label){ 
+  url_github="https://raw.githubusercontent.com/xyongcn/piazza-data-tsinghua.edu.cn_spring2015_30240243x/master/data/piazza-data-filter/filter_feed_"+label +".json";
+            $.ajax({
+                type : "get",
+                cache : false,
+                url : url_github , // 请求地址
+                success : function(data) { // ajax执行成功后执行的方法
+                    var data_json = eval("(" + data + ")"); // 把string转化为json
+                    var source_feed = $("#feed-template").html();
+                    var template_feed = Handlebars.compile(source_feed);
+                    var html_feed = template_feed(data_json);
+                    $("#feed").html(html_feed);}
+            });
+
+}
+
 
---
+++ example-together/show.js
@@ -0,0 +1,7 @@
+
+function click_select_show(value) {
+
+document.getElementById("piazza_show").src = value;
+
+}
+ 
